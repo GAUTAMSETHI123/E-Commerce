ef36134cb99c28b2b88f712c0a30e4df024aa12e
Gautam Sethi <gautam.sethi@daxko.com>
2023-07-17 13:48:19 +0530

    data annotations for primary key i.e Id

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace E_Commerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+
+namespace E_Commerce.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string DisplayOrder { get; set; }
     }
 }
